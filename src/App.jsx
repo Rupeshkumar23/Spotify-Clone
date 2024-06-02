81c621b865456f584e7279feaa91e00a6dcc2c8c
Rupeshkumar23 <coolrupesh2000@gmail.com>
2024-06-02 23:57:20 +0530

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import Sidebar from './components/Sidebar'
-import Player from './components/Player'
-import Display from './components/Display'
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Player from "./components/Player";
+import Display from "./components/Display";
 
 const App = () => {
   return (
-    <div className='h-screen bg-black'>
-      <div className='h-[90%] flex'>
-        <Sidebar/>
-        <Display/>
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
+        <Sidebar />
         
+        <Display />
       </div>
-      <Player/>
+      <Player />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
