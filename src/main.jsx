6d508b05d200d2a1523205f8ac193e3bb690c1e2
Rupeshkumar23 <coolrupesh2000@gmail.com>
2024-06-04 06:11:24 +0530

    updated code

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import PlayerContextProvider from "./context/PlayerContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <PlayerContextProvider>
+      <App />
+    </PlayerContextProvider>
+  </BrowserRouter>
+);
