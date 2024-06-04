d4dc5865635910948a342eccc4b44bf8bb5793a3
Rupeshkumar23 <coolrupesh2000@gmail.com>
2024-06-04 07:45:04 +0530

    updated code

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,7 +3,7 @@ import { assets} from "../assets/assets";
 import { PlayerContext } from "../context/PlayerContext";
 
 const Player = () => {
-  const { track, seekBar, seekBg, playStatus, play, pause } =
+  const { track, seekBar,seekSong, seekBg,time,previous,next, playStatus, play, pause } =
     useContext(PlayerContext);
   return (
     <div className="h-[10%] bg-black flex justify-between items-center text-white px-4">
@@ -22,6 +22,7 @@ const Player = () => {
             alt="shuffle_icon"
           />
           <img
+             onClick={previous}
             className="w-4 cursor-pointer"
             src={assets.prev_icon}
             alt="prev_icon"
@@ -43,6 +44,7 @@ const Player = () => {
           )}
 
           <img
+          onClick={next}
             className="w-4 cursor-pointer"
             src={assets.next_icon}
             alt="next_icon"
@@ -54,17 +56,18 @@ const Player = () => {
           />
         </div>
         <div className="flex items-center gap-5">
-          <p>1:06</p>
+          <p>{time.currentTime.minute}:{time.currentTime.second}</p>
           <div
             ref={seekBg}
+            onClick={seekSong}
             className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer"
           >
             <hr
               ref={seekBar}
-              className="h-1 border-none w-20 bg-green-800 rounded-full"
+              className="h-1 border-none w-0 bg-green-800 rounded-full"
             />
           </div>
-          <p>3:20</p>
+          <p>{time.totalTime.minute}:{time.totalTime.second}</p>
         </div>
       </div>
       <div className="hidden lg:flex items-center gap-2 opacity-75">
